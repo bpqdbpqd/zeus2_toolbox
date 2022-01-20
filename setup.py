a0ff54603ccdc71e3daaf6744152e93819840942
Christopher Rooney <NanoExplorer@users.noreply.github.com>
2022-01-20 18:57:26 -0500

    Add bs4 to dependencies
    Seems that bs4 is used by the `zeus2_io.ObsLog() .read_folder("obslogs")` method, so it might be nice to ensure it's installed automatically

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ if __name__ == "__main__":
             "numpy>=1.13",
             "astropy",
             "matplotlib",
-            "scikit-learn"
+            "scikit-learn",
+            "bs4"
         ]
     )
