Add method FigFlux.savefig() which can compress the file size by a factor of three; Fix many bugs in tools

src/zeus2_toolbox/pipeline.py
@@ -57,12 +57,23 @@ ZPOLD_SHAPE = (3, 3)  # default zpold shape, (az_len, elev_len) or (x_len, y_len
 ZPOLDBIG_SHAPE = (5, 5)  # default zpoldbig raster shape, (az_len, elev_len)
 RASTER_THRE = 2  # default SNR requirement for not flagging a pixel
 
-try:
-    TRANS_TB = Tb.read(pkgutil.get_data(__name__, "data/trans_data.csv").
+try:  # load sky transmission_raw data
+    TRANS_TB = Tb.read(pkgutil.get_data(__name__, "resources/trans_data.csv").
                        decode("utf-8"), format="ascii.csv")
-except:
-    warnings.warn("Failed to load transmission data resource.")
+except Exception as err:
+    warnings.warn("Failed to load transmission_raw data resource " +
+                  "due to %s: %s" % (type(err), err))
     TRANS_TB = None
+try:
+    COL_BIAS_TB = Tb.read(
+            pkgutil.get_data(__name__, "resources/column_line_map.csv").
+            decode("utf-8"), format="ascii.csv")
+    COL_BIAS_MAP = {col_idx: bias_idx for (col_idx, bias_idx) in
+                    zip(COL_BIAS_TB["mce_col"], COL_BIAS_TB["bias_idx"])}
+except Exception as err:
+    warnings.warn("Failed to load MCE column - bias line map resource " +
+                  "due to %s: %s" % (type(err), err))
+    COL_BIAS_MAP = None
 
 warnings.filterwarnings("ignore", message="invalid value encountered in greater")
 warnings.filterwarnings("ignore", message="invalid value encountered in less")
@@ -101,7 +112,7 @@ def check_parallel():
     return flag
 
 
-def transmission_range(freq_ran, pwv, elev=60):
+def transmission_raw_range(freq_ran, pwv, elev=60):
     """
     Compute transmission (curve) using the kappa and eta_0 data recorded in
     TRANS_TB, in the range of frequency specified in freq_ran. For the
@@ -112,16 +123,16 @@ def transmission_range(freq_ran, pwv, elev=60):
         largest and the smallest values will be interpreted as the range
     :type freq_ran: list or tuple or numpy.ndarray
     :param float pwv: float, the pwv in unit mm to compute transmission
-    :param int elev: int, the elevation in unit degree to compute
+    :param float elev: float, the elevation in unit degree to compute
         transmission
-    :return: (freq, trans), arrays recording the frequency and transmission
+    :return: (freq, trans), arrays recording the frequency and raw transmission
         computed in the given frequency range, at the input pwv and elevation
     :rtype: list
     :raises RuntimeError: transmission data not loaded
     """
 
     if TRANS_TB is None:  # check whether TRANS_TB is loaded
-        raise RuntimeError("The resource of transmission data is not loaded.")
+        raise RuntimeError("The resource of transmission_raw data is not loaded.")
 
     freq_min, freq_max = np.min(freq_ran), np.max(freq_ran)
     if np.any(freq_min < TRANS_TB["freq"].min()) or \
@@ -139,23 +150,23 @@ def transmission_range(freq_ran, pwv, elev=60):
     return freq_use.data, trans_use.data
 
 
-def transmission(freq, pwv, elev=60):
+def transmission_raw(freq, pwv, elev=60):
     """
     Compute transmission (curve) at given pwv and elevation by calling
-    transmission_range, resampled at the input freq.
+    transmission_raw_range, resampled at the input freq.
 
     :param freq: int or float or array, the frequency in unit GHz to compute
         transmission, should be within the range [400, 1610)
     :type freq: int or float or numpy.ndarray
     :param float pwv: float, the pwv in unit mm to compute transmission
-    :param int elev: int, the elevation in unit degree to compute
+    :param float elev: float, the elevation in unit degree to compute
         transmission
-    :return: variable or array recording the transmission computed at the given
-        frequency, pwv and elevation, in the same shape as input freq
+    :return: variable or array recording the raw transmission computed at the
+        given frequency, pwv and elevation, in the same shape as input freq
     :rtype: int or float or numpy.ndarray
     """
 
-    freq_use, trans_use = transmission_range(
+    freq_use, trans_use = transmission_raw_range(
             freq_ran=freq, pwv=pwv, elev=elev)
     trans = np.interp(freq, freq_use, trans_use)
 
@@ -167,7 +178,7 @@ def transmission_smoothed_range(freq_ran, pwv, elev=60, r=1000):
     Compute transmission (curve) smoothed according to the given spectral
     resolution using the kappa and eta_0 data recorded in TRANS_TB, in the range
     of frequency specified in freq_ran. The function convolves the
-    transmission curve from transmission_range() with a gaussian peak of
+    transmission curve from transmission_raw_range() with a gaussian peak of
     fwhm=freq_rep/R.
 
     :param freq_ran: list or tuple or array, the range of frequency in unit
@@ -175,7 +186,7 @@ def transmission_smoothed_range(freq_ran, pwv, elev=60, r=1000):
         largest and the smallest values will be interpreted as the range
     :type freq_ran: list or tuple or numpy.ndarray
     :param float pwv: float, the pwv in unit mm to compute transmission
-    :param int elev: int, the elevation in unit degree to compute
+    :param float elev: float, the elevation in unit degree to compute
         transmission
     :param float r: float, the spectral resolution, defining the gaussian kernel
         by fwhm=1/R*freq.mean()
@@ -196,7 +207,7 @@ def transmission_smoothed_range(freq_ran, pwv, elev=60, r=1000):
     freq_res = freq_rep / r  # FWHM of the resolution element
     flag_use = (freq_min - freq_res * 5 < TRANS_TB["freq"]) & \
                (TRANS_TB["freq"] < freq_max + freq_res * 5)
-    freq_use, trans_use = transmission_range(
+    freq_use, trans_use = transmission_raw_range(
             freq_ran=TRANS_TB["freq"][flag_use], pwv=pwv, elev=elev)
 
     gauss_kernel = gaussian(freq_use, x0=freq_use.mean(),
@@ -223,7 +234,7 @@ def transmission_smoothed(freq, pwv, elev=60, r=1000):
         resolution
     :type freq: int or float or numpy.ndarray
     :param float pwv: float, the pwv in unit mm to compute transmission
-    :param int elev: int, the elevation in unit degree to compute
+    :param float elev: float, the elevation in unit degree to compute
         transmission
     :param float r: float, the spectral resolution, defining the gaussian kernel
         by fwhm=1/r*freq.mean()
@@ -242,7 +253,7 @@ def transmission_smoothed(freq, pwv, elev=60, r=1000):
     return trans_smoothed
 
 
-def transmission_window(freq, pwv, elev=60, r=1000, d_freq=0.8):
+def transmission_pixel(freq, pwv, elev=60, r=1000, d_freq=0.8):
     """
     Because each pixel samples the energy in a certain range of frequency in the
     dispersed light, so the actual transmission is the smoothed curve
@@ -253,12 +264,12 @@ def transmission_window(freq, pwv, elev=60, r=1000, d_freq=0.8):
     width d_freq.
 
     :param freq: int or float or array, the frequency in unit GHz to compute
-        transmission, must be within the range [400, 1610); the middle value of
-        input freq will be used as the representative frequency to calculate
+        transmission, must be within the range [400, 1610); the middle value
+        of input freq will be used as the representative frequency to calculate
         resolution
     :type freq: int or float or numpy.ndarray
     :param float pwv: float, the pwv in unit mm to compute transmission
-    :param int elev: int, the elevation in unit degree to compute
+    :param float elev: float, the elevation in unit degree to compute
         transmission
     :param float r: float, the spectral resolution, defining the gaussian kernel
         by fwhm=1/r*freq.mean()
@@ -283,6 +294,53 @@ def transmission_window(freq, pwv, elev=60, r=1000, d_freq=0.8):
     trans_win = np.interp(freq, freq_use, trans_win_use)
 
     return trans_win
+
+
+def get_transmission_raw_obs_array(array_map, pwv, elev=60):
+    """
+    get an ObsArray object corresponding to the raw sky transmission of the
+    array, the array_map_ of the output obe_array object will correspond to the
+    frequency of transmission_raw_range() output instead of input array_map.
+    This function is primarily written for plotting raw transmission curve
+
+    :param ArrayMap array_map: ArrayMap object, must have wavelength initialized
+    :param float pwv: float, the pwv in unit mm to compute transmission
+    :param float elev: float, the elevation in unit degree to compute
+        transmission
+    :return: ObsArray object of transmission
+    :rtype: ObsArray
+    :raises RuntimeError: array_map wavelength not initialized
+    :raises ValueError: grat_idx not found in array_map.conf_kwargs
+    """
+
+    if not array_map.wl_flag_:
+        raise RuntimeError("array_map is not initialized with wavelength.")
+    if "grat_idx" not in array_map.conf_kwargs_:
+        raise ValueError("grat_idx not found in array_map.conf_kwargs.")
+
+    wl_edge = np.concatenate((array_map.array_wl_ - array_map.array_d_wl_ / 2,
+                              array_map.array_wl_ + array_map.array_d_wl_ / 2))
+    freq_arr, trans_arr = transmission_raw_range(
+            wl_to_freq(wl_edge), pwv=pwv, elev=elev)
+    wl_arr = freq_to_wl(freq_arr)
+    spat_arr = np.arange(array_map.array_spat_llim_,
+                         array_map.array_spat_ulim_ + 1)
+    wl = np.repeat(wl_arr, len(spat_arr))
+    trans = np.repeat(trans_arr, len(spat_arr))
+    spat = np.repeat(spat_arr[None, :], len(wl_arr), axis=0).flatten()
+    spec = wl_to_spec(wl=wl, spat=spat, **array_map.conf_kwargs_)
+
+    array_map_new = ArrayMap(np.array([spat, spec, spat, spec]).transpose())
+    obs_array_new = ObsArray(arr_in=trans[:, None], array_map=array_map_new)
+    if array_map.band_flag_:
+        with warnings.catch_warnings():
+            warnings.filterwarnings(
+                    "ignore",
+                    message="The array map is incompatible with input band.")
+            array_map_new.set_band(array_map.band_)
+        obs_array_new = obs_array_new.take_by_array_map(array_map_new)
+
+    return obs_array_new
 
 
 def gaussian_filter_obs(obs, freq_sigma=0.3, freq_center=0,
@@ -2140,7 +2198,7 @@ def read_beams(file_header_list, array_map=None, obs_log=None, flag_ts=True,
     recommended to clean the memory or start a new thread before running it in
     parallel.
 
-    :param dict file_header_list: list, of the paths to the headers of the data
+    :param list file_header_list: list, of the paths to the headers of the data
     :param ArrayMap array_map: ArrayMap, optional, if not None, will transform
         flat data into ObsArray and then process
     :param ObsLog obs_log: ObsLog, optional, if not None, will try to find the

src/zeus2_toolbox/tools.py
@@ -70,6 +70,62 @@ def gaussian_2d(pos, x0=0, y0=0, sigma_x=1, sigma_y=1, theta=0, amp=1,
            gaussian(x=yyp, x0=y0p, sigma=sigma_y, norm=norm)
 
 
+def proc_array(arr, method="mean", axis=None, **kwargs):
+    """
+    a wrapper function to process array which enables to process array by
+    specifying the function name by calling np.{method}(arr, axis=axis), also
+    expands the range of available methods including 'nanmad', 'mad',
+    'num', 'num_is_nan', 'num_not_is_nan', 'num_is_finite', 'num_not_is_finite'
+
+    :param list or tuple or numpy.ndarray arr: array to be processed
+    :param str method: str, method name to be used to process array, can either
+        be a numpy function, or method names specified in the document
+    :param int or None axis: int or None, axis to process the array, if left
+        None, the function will be applied across the whole array
+    :param dict kwargs: keyword arguments passed to np.{method}() function
+    :return: processed array
+    :rtype: numpy.ndarray
+    :raises ValueError: invalid method name
+    """
+
+    arr = np.asarray(arr)
+    func_dict = {"nanmad": lambda arr, axis: median_abs_deviation(
+            arr, axis=axis, nan_policy="omit"),
+                 "mad": lambda arr, axis: median_abs_deviation(
+                         arr, axis=axis, nan_policy="propagate"),
+                 "num": lambda arr, axis:
+                 np.count_nonzero(np.ones(arr.shape), axis=axis),
+                 "num_is_nan": lambda arr, axis:
+                 np.count_nonzero(np.isnan(arr), axis=axis),
+                 "num_not_is_nan": lambda arr, axis:
+                 np.count_nonzero(~np.isnan(arr), axis=axis),
+                 "num_is_finite": lambda arr, axis:
+                 np.count_nonzero(np.isfinite(arr), axis=axis),
+                 "num_not_is_finite": lambda arr, axis:
+                 np.count_nonzero(~np.isfinite(arr), axis=axis)}
+
+    if method in func_dict:
+        func = func_dict[method]
+    elif hasattr(np, method):
+        func = eval("np.%s" % method)
+    else:
+        raise ValueError("Input method is not a numpy function or valid name.")
+
+    try:
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", message="Mean of empty slice")
+            warnings.filterwarnings("ignore", message="All-NaN slice encountered")
+            warnings.filterwarnings("ignore", message="Mean of empty slice")
+            warnings.filterwarnings(
+                    "ignore", message="Degrees of freedom <= 0 for slice.")
+            arr_proc = func(arr, axis=axis, **kwargs)
+    except Exception as err:
+        raise RuntimeError("Failed to call %s with %s." % (method, func)) \
+            from err
+
+    return arr_proc
+
+
 def weighted_mean(arr, wt=None, nan_policy="omit"):
     """
     Calculate the weighted mean of 1-d array, return the flux, error and summed
@@ -280,12 +336,12 @@ def nanmad_flag(arr, thre=20, axis=-1):
     :param float thre: float, data with abs distance > thre*MAD will be flagged
     :param int axis: int, axis along which the mad will be checked, if input is
         None, will use the median of the whole array
-    :return flag_arr: array, bool values of flag
+    :return flag_arr: array, bool values of flag in the same shape as arr
     :rtype: numpy.ndarray
     :raises ValueError: invalid axis value
     """
 
-    arr = np.array(arr)  # check input arr and axis
+    arr = np.asarray(arr)  # check input arr and axis
     ndim, shape = len(arr.shape), arr.shape
     if axis is not None:
         if int(axis) not in range(-ndim, ndim):  # check axis value
@@ -293,22 +349,17 @@ def nanmad_flag(arr, thre=20, axis=-1):
         axis = int(axis) if (axis >= 0) else int(arr.ndim + axis)
 
     with warnings.catch_warnings():
-        warnings.filterwarnings(
-                "ignore", message="All-NaN slice encountered")
-        warnings.filterwarnings(
-                "ignore", message="Mean of empty slice")
-        warnings.filterwarnings(
-                "ignore", message="invalid value encountered in greater")
-        if ((np.isfinite(arr)).sum() == 0) and (axis is None):
-            med = np.full(arr.shape, fill_value=np.nan)
+        warnings.filterwarnings("ignore", message="All-NaN slice encountered")
+        warnings.filterwarnings("ignore", message="Mean of empty slice")
+        warnings.filterwarnings("ignore",
+                                message="invalid value encountered in greater")
+        if (proc_array(arr, method="num_is_finite") == 0) and (axis is None):
+            flag_arr = np.full(arr.shape, fill_value=False)
         else:
             med = np.nanmedian(arr, axis=axis, keepdims=True)
-        abs_div = np.abs(arr - med)
-        if ((np.isfinite(arr)).sum() == 0) and (axis is None):
-            mad = np.full(arr.shape, fill_value=np.nan)
-        else:
+            abs_div = np.abs(arr - med)
             mad = np.nanmedian(abs_div, axis=axis, keepdims=True)
-        flag_arr = (abs_div > mad * thre)
+            flag_arr = (abs_div > mad * thre)
 
     return flag_arr
 
@@ -318,7 +369,7 @@ def double_nanmad_flag(arr, thre=20, axis=-1, frac_thre=0.1):
     Similar to nanmad_flag(), but do nanmad_flag a second time on the data flagged
     by nanmad_flag(), if frac_thre of data is flagged in the first time in a
     certain entry of arr, to account for the sudden jump often witnessed in time
-    series
+    series or bimodal distribution
 
     :param numpy.ndarray arr: array, to be checked
     :param float thre: float, data with abs distance > thre*MAD will be flagged
@@ -335,19 +386,19 @@ def double_nanmad_flag(arr, thre=20, axis=-1, frac_thre=0.1):
     if not 0 <= frac_thre <= 1:
         raise ValueError("Invalid value for frac_thre.")
 
-    arr = np.array(arr)  # check input arr and axis
-    first_flag_arr = nanmad_flag(arr=arr, thre=thre, axis=axis)
+    arr = np.asarray(arr)  # check input arr and axis
+    flag_arr = nanmad_flag(arr=arr, thre=thre, axis=axis)
     flagged_arr = np.full_like(arr, fill_value=np.nan)
+
     with warnings.catch_warnings():
         warnings.filterwarnings(
                 "ignore", message="invalid value encountered in greater")
-        do_flag = (np.nansum(first_flag_arr, axis=axis, keepdims=True) /
-                   first_flag_arr.shape[axis]) > frac_thre
-    flagged_arr[first_flag_arr & do_flag] = arr[first_flag_arr & do_flag]
+        do_flag = (np.nansum(flag_arr, axis=axis, keepdims=True) /
+                   flag_arr.shape[axis]) > frac_thre
+    np.putmask(flagged_arr, flag_arr & do_flag, arr)
     second_flag_arr = nanmad_flag(arr=flagged_arr, thre=thre, axis=axis)
-    flag_arr = first_flag_arr
-    flag_arr[np.full_like(first_flag_arr, fill_value=True) & do_flag] = \
-        second_flag_arr[np.full_like(first_flag_arr, fill_value=True) & do_flag]
+    np.putmask(flag_arr, np.full_like(flag_arr, fill_value=True) & do_flag,
+               second_flag_arr)
 
     return flag_arr
 
@@ -368,10 +419,12 @@ def naninterp(x, xp, fp, fill_value=np.nan):
     """
 
     finite_mask = np.isfinite(xp) & np.isfinite(fp)
-    if finite_mask.sum() == 0:
-        return np.full(x.shape, fill_value=fill_value)
+    if np.count_nonzero(finite_mask) == 0:
+        result = np.full(x.shape, fill_value=fill_value)
     else:
-        return np.interp(x, xp[finite_mask], fp[finite_mask])
+        result = np.interp(x, xp[finite_mask], fp[finite_mask])
+
+    return result
 
 
 def nanlstsq(a, b, rcond=None, fill_value=np.nan):
@@ -387,11 +440,13 @@ def nanlstsq(a, b, rcond=None, fill_value=np.nan):
     """
 
     finite_flag_arr = np.all(np.isfinite(a), axis=-1) & np.isfinite(b)
-    if len(finite_flag_arr) == 0:
+    if np.count_nonzero(finite_flag_arr) == 0:
         lstsq_0 = lstsq(np.zeros(a.shape), np.zeros(b.shape), rcond=rcond)
-        return (np.full(lstsq_0[0].shape, fill_value=fill_value),) + lstsq_0[1:]
+        result = (np.full(lstsq_0[0].shape, fill_value=fill_value),) + lstsq_0[1:]
     else:
-        return lstsq(a[finite_flag_arr, :], b[finite_flag_arr], rcond=rcond)
+        result = lstsq(a[finite_flag_arr, :], b[finite_flag_arr], rcond=rcond)
+
+    return result
 
 
 def check_orientation(orientation):

src/zeus2_toolbox/view.py
@@ -9,6 +9,8 @@ Visualization of data
 from matplotlib import cm, colors, font_manager
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+from PIL import Image
+from io import BytesIO
 
 from .zeus2_io import *
 
@@ -127,8 +129,14 @@ class FigFlux(Figure):
                 extent = get_corr_extent(self.extent_)
             self.__init_main_axes__(extent=extent)
         ax = self.main_axes_
-        ax.imshow(arr, cmap=self.cmap_, norm=self.norm_, interpolation="none",
-                  origin="upper", extent=extent, aspect="auto", **kwargs)
+        use_kwargs = kwargs.copy()
+        default_kwargs = {"cmap": self.cmap_, "norm": self.norm_,
+                          "interpolation": "none", "origin": "upper",
+                          "extent": extent, "aspect": "auto"}
+        for key in default_kwargs:
+            if key not in use_kwargs:
+                use_kwargs[key] = default_kwargs[key]
+        ax.imshow(arr, **use_kwargs)
         self.__set_ticks__()
 
     def __plot_colorbar__(self, **kwargs):
@@ -241,6 +249,8 @@ class FigFlux(Figure):
             ran = np.nanmax(abs(arr[~arr_mask]))
 
         # transform data to RGBA
+        if "cmap" in kwargs:
+            self.cmap_ = plt.get_cmap(kwargs["cmap"])
         self.norm_ = colors.Normalize(vmin=-ran, vmax=ran)
         flag_nan = np.isnan(arr)
         np.putmask(arr, np.isnan(arr), 0)  # replace nan by 0
@@ -314,6 +324,34 @@ class FigFlux(Figure):
                 ax.text(x=extent_round[0] + col - 0.3,
                         y=extent_round[2] + row + 0.3,
                         s=arr[row, col], fontsize=self.text_fontsize_, **kwargs)
+
+    def savefig(self, fname, *args, compress=True, **kwargs):
+        """
+        instead of using the default savefig function in matplotlib, this
+        function can convert the Figure object to PIL.Image.Image object,
+        converting the mode from "RGB" to "P" which reduces the file size by a
+        factor of three, and then save the figure to the specified filename
+
+        :param str fname: str, filename, passed to
+            matplotlib.figure.Figure.savefig()
+        :param bool compress: bool flag whether to do compress size, default True
+        """
+
+        if not compress:
+            return super(FigFlux, self).savefig(fname=fname, *args, **kwargs)
+        else:
+            save_kwarg, fmt = kwargs.copy(), None
+            if "format" in save_kwarg:
+                fmt = save_kwarg["format"]
+                save_kwarg.pop("format")
+            with BytesIO() as buffer:  # write to memory
+                super(FigFlux, self).savefig(buffer, *args, **save_kwarg)
+                buffer.seek(0)
+                with Image.open(buffer) as im:
+                    im2 = im.convert('RGB').convert(
+                            'P', palette=Image.Palette.ADAPTIVE)
+                    return im2.save(fp=fname, format=fmt, optimize=True,
+                                    quality=50)
 
     def set_title(self, title, fontsize=None):
         """
@@ -788,7 +826,7 @@ class FigArray(FigFlux):
         :param xlim: xlim to set for individual (Obs/ObsArray input) or all
             (tuple input) pixel axes. If left None, will pick the xlim suitable
             xlim for all pixel axes
-        :type xlim: tuple or list or Obs or ObsArray
+        :type xlim: tuple or list or Obs or ObsArray or None
         :raises ValueError: axes list not initialized
         :raises TypeError: invalid xlim type
         """
@@ -830,7 +868,7 @@ class FigArray(FigFlux):
         :param ylim: ylim to set for individual (Obs/ObsArray input) or all
             (tuple input) pixel axes. If left None, will pick the ylim suitable
             ylim for all pixel axes
-        :type ylim: tuple or list or Obs or ObsArray
+        :type ylim: tuple or list or Obs or ObsArray or None
         :param bool twin_axes: bool flag, whether to apply on the secondary
             y-axis
         :raises ValueError: axes list not initialized
@@ -1606,11 +1644,11 @@ class FigSpec(FigFlux):
         y = 1 - self.fontsize_ * 4.6 / 72 / figsize[1] - dy
 
         axs_list, spat_list = [], []
-        for spat in range(extent_round[2], extent_round[3] + 1):
+        for spat in np.arange(extent_round[2], extent_round[3] + 1):
             axp = self.add_axes(
                     (x, y - (spat - extent_round[2]) * self.y_size_ / figsize[1],
                      dx, dy), zorder=10)
-            axp.patch.set_alpha(0.7)
+            axp.patch.set_alpha(0.4)
             axp.tick_params(axis="both", direction="in",
                             bottom=True, top=True, left=True, right=True,
                             labelbottom=False, labeltop=False)
@@ -1637,6 +1675,9 @@ class FigSpec(FigFlux):
             twin_ax = ax.twinx()
             twin_ax.tick_params(axis="both", direction="in",
                                 bottom=True, top=True, left=True, right=True)
+            for item in ([twin_ax.yaxis.label, twin_ax.yaxis.offsetText] +
+                         twin_ax.get_yticklabels()):
+                item.set_fontsize(self.axs_fontsize_)
             twin_axs_list.append(twin_ax)
         self.twin_axs_list_ = twin_axs_list
 
@@ -1806,6 +1847,55 @@ class FigSpec(FigFlux):
             kwargs["fontsize"] = self.axs_fontsize_
         ax.legend(*args, **kwargs)
 
+    def plot(self, obs_array, *args, mask=None, pix_flag_list=None,
+             twin_axes=False, **kwargs):
+        """
+        plot input obs_array on the axes of obs_array.array_map_.array_spec_ on
+        each spat, calling axes.plot()
+
+        :param ObsArray obs_array: ObsArray, object to plot, should be length 1
+            if ts is initialized
+        :param args: arguments passed to axes.plot()
+        :param numpy.ndarray mask: bool mask of flagged pixels, should have the
+            same shape as obs_array
+        :param list pix_flag_list: list of (spat, spec) of flagged pixels
+        :param bool twin_axes: bool flag, whether to plot using the secondary
+            y-axis
+        :param kwargs: keyword arguments passed to axes.plot()
+        """
+
+        obs_array = ObsArray(arr_in=obs_array)
+        if (obs_array.ts_.empty_flag_ and obs_array.ndim_ > 1 and
+            obs_array.len_ > 1) or (not obs_array.ts_.empty_flag_ and
+                                    ((obs_array.ndim_ > 2) or
+                                     (obs_array.len_ > 1))):
+            raise ValueError("Input obs_array should have length 1.")
+        array_map = obs_array.array_map_
+        if mask is None:
+            mask = np.full(obs_array.shape_, fill_value=False, dtype=bool)
+        mask_obs = ObsArray(arr_in=mask)
+        mask_obs.update_array_map(array_map)
+        pix_flag = array_map.get_flag_where(spat_spec_list=pix_flag_list)
+        mask_obs.fill_by_flag_along_axis(pix_flag, axis=0, fill_value=True)
+
+        if self.axs_list_ is None:
+            self.__init_axs_list__(array_map=array_map)
+        if twin_axes:
+            if self.twin_axs_list_ is None:
+                self.__init_twin_axs_list__()
+            axs_use = self.twin_axs_list_
+        else:
+            axs_use = self.axs_list_
+
+        for spat, ax in zip(self.spat_list_, axs_use):
+            array_map_use = array_map.take_where(spat=spat)
+            if not array_map_use.empty_flag_:
+                x, data = get_spat_data(
+                        obs_array=obs_array, spat=spat, mask_obs=mask_obs)
+                data = data.flatten()
+                if np.count_nonzero(np.isfinite(data)) > 0:
+                    ax.plot(x, data, *args, **kwargs)
+
     def step(self, obs_array, *args, where='mid', mask=None, pix_flag_list=None,
              twin_axes=False, **kwargs):
         """
@@ -1813,6 +1903,7 @@ class FigSpec(FigFlux):
 
         :param ObsArray obs_array: ObsArray, object to plot, should be length 1
             if ts is initialized
+        :param args: arguments passed to axes.step()
         :param str where: string of step position passed to axes.step()
         :param numpy.ndarray mask: bool mask of flagged pixels, should have the
             same shape as obs_array
@@ -1905,34 +1996,23 @@ class FigSpec(FigFlux):
             axs_use = self.twin_axs_list_
         else:
             axs_use = self.axs_list_
-        x = np.arange(array_map.array_spec_llim_,
-                      array_map.array_spec_ulim_ + 1)
 
         for spat, ax in zip(self.spat_list_, axs_use):
-            spec = np.full(len(x), fill_value=np.nan, dtype=float)
             array_map_use = array_map.take_where(spat=spat)
             if not array_map_use.empty_flag_:
-                spec_idxs = array_map_use.array_spec_
-                data_use = obs_array.take_by_array_map(array_map_use).data_. \
-                    flatten()
-                mask_use = mask_obs.take_by_array_map(array_map_use).data_. \
-                    flatten()
-                spec[spec_idxs[~mask_use] - array_map.array_spec_llim_] = \
-                    data_use[~mask_use]
+                x, spec = get_spat_data(
+                        obs_array=obs_array, spat=spat, mask_obs=mask_obs)
+                spec = spec.flatten()
                 if xerr_obs is not None:
-                    spec_xerr = np.full(len(x), fill_value=np.nan, dtype=float)
-                    xerr_use = xerr_obs.take_by_array_map(array_map_use). \
-                        data_.flatten()
-                    spec_xerr[spec_idxs[~mask_use] -
-                              array_map.array_spec_llim_] = xerr_use[~mask_use]
+                    (_, spec_xerr) = get_spat_data(
+                            obs_array=xerr_obs, spat=spat, mask_obs=mask_obs)
+                    spec_xerr = spec_xerr.flatten()
                 else:
                     spec_xerr = None
                 if yerr_obs is not None:
-                    spec_yerr = np.full(len(x), fill_value=np.nan, dtype=float)
-                    yerr_use = yerr_obs.take_by_array_map(array_map_use). \
-                        data_.flatten()
-                    spec_yerr[spec_idxs[~mask_use] -
-                              array_map.array_spec_llim_] = yerr_use[~mask_use]
+                    (_, spec_yerr) = get_spat_data(
+                            obs_array=yerr_obs, spat=spat, mask_obs=mask_obs)
+                    spec_yerr = spec_yerr.flatten()
                 else:
                     spec_yerr = None
                 if np.count_nonzero(np.isfinite(spec)) > 0:
@@ -2169,3 +2249,25 @@ def get_chop_color(chop):
         color = "black"
 
     return color
+
+
+def get_spat_data(obs_array, spat, mask_obs=None):
+    """
+    return the spec and data recorded at the given spat
+    """
+
+    array_map_use = obs_array.array_map_.take_where(spat=spat).sort("spec")
+
+    x = array_map_use.array_spec_
+    data_use = obs_array.take_by_array_map(array_map_use).data_
+    data = np.full(data_use.shape, fill_value=np.nan, dtype=float)
+
+    if mask_obs is not None:
+        mask_use = mask_obs.take_by_array_map(array_map_use).data_
+        np.putmask(data, ~mask_use, data_use)
+    else:
+        data = data_use
+    if data.ndim > 1:
+        data = data.transpose()
+
+    return x, data